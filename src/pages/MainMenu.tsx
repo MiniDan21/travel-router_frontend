fbd8a866c30b1beb62e3b2ca3a03d30eb8d1aebd
MiniDan21 <molchanov_danilka00@mail.ru>
2025-08-10 01:27:48 +0300

    Added structure for beginning

---
+++ src/pages/MainMenu.tsx
@@ -0,0 +1,10 @@
+import MenuBar from './components/MenuBar'
+import MainWindow from './components/MainWindow'
+
+export default function MainMenu() {
+    return <>
+            <MenuBar/>
+            {/* Главное меню с картой и сайд-барами */}
+            <MainWindow/>
+        </>
+}
---
+++ src/pages/components/MainWindow.tsx
@@ -0,0 +1,9 @@
+import SideBar from "./SideBar"
+import MapCanvas from "./MapCanvas"
+
+export default function MainWindow() {
+    return <>
+        <SideBar/>
+        <MapCanvas/>
+    </>
+}
